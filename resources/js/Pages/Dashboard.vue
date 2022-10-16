76880819929dc223d820daf97e63fe845efd5a41
NichoAdhyatma <nicholausadhyatna@gmail.com>
2022-10-16 16:57:12 +0700

    add dark mode

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,12 +1,17 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
+
+const props = defineProps({
+    user: Object,
+});
+
 </script>
 
 <template>
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="font-semibold text-xl leading-tight">
                 Dashboard
             </h2>
         </template>
@@ -14,7 +19,7 @@ import Welcome from '@/Components/Welcome.vue';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    <Welcome :username="user.name"/>
                 </div>
             </div>
         </div>
---
+++ resources/js/Pages/MyOrmawa.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+</script>
+
+<template>
+  <AppLayout title="My-Ormawa">
+    <template #header>
+      <h2 class="font-semibold text-xl leading-tight">
+        My Ormawa
+      </h2>
+    </template>
+  </AppLayout>
+</template>
